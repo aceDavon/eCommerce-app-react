6d36d9df94633a31f05016cd18d2efed1d9991d1
acedavon <makojidavid@gmail.com>
2022-05-15 21:04:33 +0100

    Replace multiple useState occurrences with a single useReducer
    optimize code for a clean experience, Replace multiple useState with a
    single useReducer on the navbar

--- src/components/BaseComponents/Navbar.jsx
+++ src/components/BaseComponents/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { ReactComponent as Logo } from "../img/wplus-icon-white.svg";
 import { ReactComponent as Box } from "../img/icons/circle.svg";
 import { ReactComponent as Reorder } from "../img/icons/reorder.svg";
@@ -7,24 +7,48 @@ import Departments from "./Departments";
 import { MyItems } from "./MyItems";
 import { Accounts } from "./Accounts";
 
-export const Navbar = () => {
-    const [dept, setDept] = useState(false);
-    const [serv, setServ] = useState(false);
-    const [items, setItems] = useState(false);
-    const [acct, setAcct] = useState(false);
+const Reducer = (state, action) => {
+  switch (action.type) {
+    case "dept":
+      return {
+        dept: !state.dept,
+        serv: state.serv,
+        items: state.items,
+        acct: state.acct,
+      };
+    case "serv":
+      return {
+        dept: state.dept,
+        serv: !state.serv,
+        items: state.items,
+        acct: state.acct,
+      };
+    case "items":
+      return {
+        dept: state.dept,
+        serv: state.serv,
+        items: !state.items,
+        acct: state.acct,
+      };
+    case "acct":
+      return {
+        dept: state.dept,
+        serv: state.serv,
+        items: state.items,
+        acct: !state.acct,
+      };
+    default:
+      return state;
+  }
+};
 
-    const deptHover = () => {
-        setDept(!dept)
-    };
-    const servHover = () => {
-      setServ(!serv);
-    };
-    const itemsHover = () => {
-      setItems(!items);
-    };
-    const acctHover = () => {
-      setAcct(!acct);
-    };
+export const Navbar = () => {
+  const [state, dispatch] = useReducer(Reducer, {
+    dept: false,
+    serv: false,
+    items: false,
+    acct: false,
+  });
 
   return (
     <nav className="w-full flex justify-between gap-12 items-center px-5 bg-blue-500 py-2">
@@ -35,24 +59,24 @@ export const Navbar = () => {
       <div className="w-11/12 flex gap-1 items-center">
         <div
           className=" w-1/6 list flex items-center relative py-2 hover:bg-blue-700 bg-transparent rounded-lg"
-          onMouseEnter={deptHover}
-          onMouseLeave={deptHover}
+          onMouseEnter={() => dispatch({ type: "dept" })}
+          onMouseLeave={() => dispatch({ type: "dept" })}
         >
           <Box className="w-3 fill-white hover:fill-black text-white" />
           <div>
             <span className="text-white capitalize">Departments</span>
-            {dept && <Departments />}
+            {state.dept && <Departments />}
           </div>
         </div>
         <div
           className="w-1/6 list flex gap-1 items-center relative py-2 hover:bg-blue-700 bg-transparent rounded-lg"
-          onMouseEnter={servHover}
-          onMouseLeave={servHover}
+          onMouseEnter={() => dispatch({ type: "serv" })}
+          onMouseLeave={() => dispatch({ type: "serv" })}
         >
           <Box className="w-3 fill-white hover:fill-black text-white" />
           <div>
             <span className="text-white capitalize">services</span>
-            {serv && <Departments />}
+            {state.serv && <Departments />}
           </div>
         </div>
         <div className="w-3/5">
@@ -63,8 +87,8 @@ export const Navbar = () => {
         </div>
         <div
           className="w-1/6 list flex gap-1 items-center relative hover:bg-blue-700 bg-transparent rounded-lg"
-          onMouseEnter={itemsHover}
-          onMouseLeave={itemsHover}
+          onMouseEnter={() => dispatch({ type: "items" })}
+          onMouseLeave={() => dispatch({ type: "items" })}
         >
           <Reorder className="w-3 h-3 fill-white hover:fill-black text-white" />
           <div className="flex flex-col items-end px-1">
@@ -72,13 +96,13 @@ export const Navbar = () => {
               reorder
             </span>
             <span className="text-white capitalize">my items</span>
-            {items && <MyItems />}
+            {state.items && <MyItems />}
           </div>
         </div>
         <div
           className="w-1/6 list flex gap-1 items-center relative hover:bg-blue-700 bg-transparent rounded-lg"
-          onMouseEnter={acctHover}
-          onMouseLeave={acctHover}
+          onMouseEnter={() => dispatch({ type: "acct" })}
+          onMouseLeave={() => dispatch({ type: "acct" })}
         >
           <Acct className="w-3 h-3 fill-white hover:fill-black text-white" />
           <div className="flex flex-col items-end px-1">
@@ -86,7 +110,7 @@ export const Navbar = () => {
               signin
             </span>
             <span className="text-white capitalize">Accounts</span>
-            {acct && <Accounts />}
+            {state.acct && <Accounts />}
           </div>
         </div>
       </div>
